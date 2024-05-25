520cc7556dd9e6395c8fbc3f0fbdf3bd72415f45
EunjiShin <eunji980310@gmail.com>
2024-05-25 23:50:32 +0900

    feat: suspend fun

---
+++ playground/src/main/kotlin/SuspendExample.kt
@@ -0,0 +1,34 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+// suspension point를 만드는 delay는 코루틴 또는 suspend fun 내에서만 호출할 수 있다.
+suspend fun doThree() {
+    println("launch1 : ${Thread.currentThread().name}")
+    delay(1000L)
+    println("3!")
+}
+
+suspend fun doTwo() {
+    println("runBlocking : ${Thread.currentThread().name}")
+    delay(500L)
+    println("2!")
+}
+
+// delay가 없어서 suspend modifier를 굳이 붙일 필요가 없음
+fun doOne() {
+    println("launch1 : ${Thread.currentThread().name}")
+    println("1!")
+}
+
+// runBlocking<Int> 등을 이용해 코루틴 반환타입을 지정할 수 있다.
+fun main() = runBlocking<Unit> {
+    launch {
+        doThree()
+    }
+    launch {
+        doOne()
+    }
+    doTwo()
+    3
+}
